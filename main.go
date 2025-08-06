b0a78e64e86012be2d32b68d7adf4f2758a222dd
firemanm <firemanm@gmail.com>
2025-08-06 22:26:02 +0000

    -

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		resp := make(map[string]string)
-		resp["message"] = "Status OK"
+		resp["Status"] = "OK"
 		jsonResp, err := json.Marshal(resp)
 		if err != nil {
 			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
